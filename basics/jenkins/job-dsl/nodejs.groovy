771a4aaeb46b533a5cddeeba1a77bf9eaad568f9
Kolikant <a.kolikant@gmail.com>
2019-08-07 16:27:06 +0300

    add jenkinsfile v2 usage

--- basics/jenkins/job-dsl/nodejs.groovy
+++ basics/jenkins/job-dsl/nodejs.groovy
@@ -68,3 +68,26 @@ pipelineJob('DSL_Pipeline') {
     }
   }
 }
+
+pipelineJob('DSL_Pipeline2') {
+
+  def repo = "https://github.com/Kolikant/docker-cicd.git"
+
+  triggers {
+    scm('H/5 * * * *')
+  }
+  description("Pipeline for $repo")
+
+  definition {
+    cpsScm {
+      scm {
+        git {
+          remote { url(repo) }
+          branches('master')
+          scriptPath('./basics/misc/Jenkinsfile.v2')
+          extensions { }  // required as otherwise it may try to tag the repo, which you may not want
+        }
+      }
+    }
+  }
+}
